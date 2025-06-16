0a17c5f4fd0b1b6bd761d429836f4f1ebbde3949
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-06-16 17:30:02 +0200

    add show route with reviews

--- controller/movieController.js
+++ controller/movieController.js
@@ -4,11 +4,44 @@ const index = (req, res) => {
   const sql = "SELECT * FROM movies";
   connection.query(sql, (err, results) => {
     if (err)
-      return res.status(502).json({
+      return res.status(500).json({
         error: "invalid request",
       });
     res.json(results);
   });
 };
 
-module.exports = { index };
+const show = (req, res) => {
+  const sql = "SELECT * FROM movies WHERE id = ?";
+  const reviewSql = "SELECT * FROM reviews WHERE id = ?";
+  const { id } = req.params;
+  connection.query(sql, [id], (err, results) => {
+    if (err)
+      return res.status(500).json({
+        error: "invalid request",
+      });
+    if (results.length === 0)
+      return res.status(404).json({
+        error: "Film non trovato",
+      });
+
+    const movie = results[0];
+
+    connection.query(reviewSql, [id], (err, results) => {
+      if (err)
+        return res.status(500).json({
+          error: "invalid request",
+        });
+      if (results.length === 0)
+        return res.status(404).json({
+          error: "Film non trovato",
+        });
+      res.json({
+        movie,
+        reviews: results[0],
+      });
+    });
+  });
+};
+
+module.exports = { index, show };
