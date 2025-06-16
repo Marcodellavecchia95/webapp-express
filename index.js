8036c9ff5eb535199ab4c5c3e6c1d927c2a10916
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-06-16 16:33:11 +0200

    add index route

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ require("dotenv").config();
 const app = express();
 const port = process.env.API_PORT;
 const apiUrl = process.env.API_URL;
+const connection = require("./data/db");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM movies";
+  connection.query(sql, (err, results) => {
+    if (err)
+      return res.status(502).json({
+        error: "invalid request",
+      });
+    res.json(results);
+  });
+});
 
 app.listen(port, () => {
   console.log(`server listening on ${apiUrl}${port}`);
