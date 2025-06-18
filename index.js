b935cfa14bb822f667b50b12cd344dc296b0b69f
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-06-18 14:17:48 +0200

    installed cors

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.API_PORT;
 const apiUrl = process.env.API_URL;
+const apiUrlFrontend = process.env.FRONTEND_URL;
 const movieRouter = require("./routers/router");
 const errorHandler = require("./middlewares/errorHandler");
 const notFoundHandler = require("./middlewares/notFoundHandler");
+const corsOptions = {
+  origin: apiUrlFrontend,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
